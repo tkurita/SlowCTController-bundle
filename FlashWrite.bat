b1fade75bab55e4cd07e8a4723facb4a8c09ec1e
Tetsuro Kurita <tkurita@werc.or.jp>
2012-12-06 11:55:02 +0900

    initial commit

---
+++ FlashWrite.bat
@@ -0,0 +1,3 @@
+cd flashwriter
+C:\CCStudio_v3.1\cc\bin\cc_app.exe autorun.gel
+
---
+++ RunSlowCTController.bat
@@ -0,0 +1,4 @@
+start C:\CCStudio_v3.1\cc\bin\cc_app.exe CT-DSP\autorun.gel
+rem cd CT-DSP\\Debug
+rem java -jar ../../SlowCTController/dist/SlowCTController.jar
+start javaw -jar SlowCTController/dist/SlowCTController.jar
