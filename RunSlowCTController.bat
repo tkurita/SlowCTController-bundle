17435b7403dccc8f24f61a7ddbf8642bb439bd7c
Tetsuro Kurita <tkurita@werc.or.jp>
2012-12-06 15:16:33 +0900

    added Load CT-DSP, Launch SlowCTController

--- RunSlowCTController.bat
+++ RunSlowCTController.bat
@@ -1,4 +1,2 @@
 start C:\CCStudio_v3.1\cc\bin\cc_app.exe CT-DSP\autorun.gel
-rem cd CT-DSP\\Debug
-rem java -jar ../../SlowCTController/dist/SlowCTController.jar
 start javaw -jar SlowCTController/dist/SlowCTController.jar
